aef8a3378efc0b02c38bd3fa4c93f97d7af27e5b
testgithubritgu <swapnilraut1698@gmail.com>
2025-08-21 12:46:47 +0530

    Added React frontend files properly

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <>
+    <div>
+     <Routes>
+      <Route path='/' />
+      </Routes>
+    </div>
+    </>
+  )
+}
+
+export default App
